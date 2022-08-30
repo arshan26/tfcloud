490058373d1bc76d032f62aecd6aaff2f69b9de2
arshan26 <mohamed.arshan@studiographene.com>
2022-08-30 10:57:48 +0530

    updated4

--- main.tf
+++ main.tf
@@ -1,17 +1,17 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
+# terraform {
+#   required_providers {
+#     aws = {
+#       source  = "hashicorp/aws"
+#       version = "~> 4.0"
+#     }
+#   }
+# }
 
 //
-# provider "aws" {
-#   region = "us-east-1"
+provider "aws" {
+  region = "us-east-1"
   
-# }
+}
 
 
 resource "aws_vpc" "my_vpc" {
