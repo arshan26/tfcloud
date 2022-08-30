e88dfa1a67bff267f761c6970094a4b703224518
arshan26 <mohamed.arshan@studiographene.com>
2022-08-30 14:22:15 +0530

    update5

---
+++ install.sh
@@ -0,0 +1,5 @@
+sudo yum update -y
+sudo yum install -y httpd
+sudo systemctl enable httpd
+sudo service httpd start
+echo "<h1>This is terraform cloud demo using remote state file" | sudo tee /var/www/html/index.html
