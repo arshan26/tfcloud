924edd60532644efc3b252338d2a2b9011c5fa49
arshan26 <mohamed.arshan@studiographene.com>
2022-08-30 15:18:51 +0530

    update6.2

--- security-group.tf
+++ security-group.tf
@@ -3,7 +3,7 @@
 #---
 
 resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
+  name        = "SG"
   description = "Allow TLS inbound traffic"
   vpc_id      = aws_vpc.my_vpc.id
 
