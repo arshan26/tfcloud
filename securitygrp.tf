586a83af4ced340a064c1172aa6a8fb819f2d54e
arshan26 <mohamed.arshan@studiographene.com>
2022-08-30 15:05:33 +0530

    update6.1

--- securitygrp.tf
+++ securitygrp.tf
@@ -5,7 +5,7 @@
 resource "aws_security_group" "allow_tls" {
   name        = "allow_tls"
   description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
+  vpc_id      = aws_vpc.my_vpc.id
 
   ingress {
     description = "allow 22 port"
