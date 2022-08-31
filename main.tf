0629cbbd0dffdca6860b837717892add6d2a47f6
Mohamed arshan <110810348+arshan26@users.noreply.github.com>
2022-08-31 17:46:36 +0530

    update7.2

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ resource "aws_instance" "foo1" {
   subnet_id = aws_subnet.my_subnet.id
 
   network_interface {
-     network_interface_id = aws_network_interface.foo1.id
+     network_interface_id = aws_network_interface.foo.id
      device_index         = 0
   }
 
